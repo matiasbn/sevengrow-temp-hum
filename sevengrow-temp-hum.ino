75b4dfb5c71ace1dfaa177215e6bd5079766c4d1
Matias <matias.barriosn@gmail.com>
2019-03-09 18:21:12 -0300

    Starting point to WiFi connection and DHT11

--- sevengrow-temp-hum.ino
+++ sevengrow-temp-hum.ino
@@ -8,11 +8,9 @@
 
 #include <ESP8266WiFi.h>
 #include <SimpleDHT.h>
-#include <PubSubClient.h>
 
 const char *ssid = "VTR-2778044";
 const char *password = "dnn6jtjpXvSq";
-WiFiClient client;
 
 // for DHT11,
 //      VCC: 5V or 3V
@@ -23,21 +21,14 @@ SimpleDHT11 dht11(pinDHT11);
 
 int value = 0;
 
-const char *ssid = "........";
-const char *password = "........";
-const char *mqtt_server = "broker.mqtt-dashboard.com";
-
-WiFiClient espClient;
-PubSubClient client(espClient);
-long lastMsg = 0;
-char msg[50];
-int value = 0;
-
-void setup_wifi()
+void setup()
 {
-
+    Serial.begin(115200);
     delay(10);
+
     // We start by connecting to a WiFi network
+
+    Serial.println();
     Serial.println();
     Serial.print("Connecting to ");
     Serial.println(ssid);
@@ -50,74 +41,10 @@ void setup_wifi()
         Serial.print(".");
     }
 
-    randomSeed(micros());
-
     Serial.println("");
     Serial.println("WiFi connected");
     Serial.println("IP address: ");
     Serial.println(WiFi.localIP());
-}
-
-void callback(char *topic, byte *payload, unsigned int length)
-{
-    Serial.print("Message arrived [");
-    Serial.print(topic);
-    Serial.print("] ");
-    for (int i = 0; i < length; i++)
-    {
-        Serial.print((char)payload[i]);
-    }
-    Serial.println();
-
-    // Switch on the LED if an 1 was received as first character
-    if ((char)payload[0] == '1')
-    {
-        digitalWrite(BUILTIN_LED, LOW); // Turn the LED on (Note that LOW is the voltage level
-                                        // but actually the LED is on; this is because
-                                        // it is active low on the ESP-01)
-    }
-    else
-    {
-        digitalWrite(BUILTIN_LED, HIGH); // Turn the LED off by making the voltage HIGH
-    }
-}
-
-void reconnect()
-{
-    // Loop until we're reconnected
-    while (!client.connected())
-    {
-        Serial.print("Attempting MQTT connection...");
-        // Create a random client ID
-        String clientId = "ESP8266Client-";
-        clientId += String(random(0xffff), HEX);
-        // Attempt to connect
-        if (client.connect(clientId.c_str()))
-        {
-            Serial.println("connected");
-            // Once connected, publish an announcement...
-            client.publish("outTopic", "hello world");
-            // ... and resubscribe
-            client.subscribe("inTopic");
-        }
-        else
-        {
-            Serial.print("failed, rc=");
-            Serial.print(client.state());
-            Serial.println(" try again in 5 seconds");
-            // Wait 5 seconds before retrying
-            delay(5000);
-        }
-    }
-}
-
-void setup()
-{
-    pinMode(BUILTIN_LED, OUTPUT); // Initialize the BUILTIN_LED pin as an output
-    Serial.begin(115200);
-    setup_wifi();
-    client.setServer(mqtt_server, 1883);
-    client.setCallback(callback);
 }
 
 void loop()
@@ -148,24 +75,8 @@ void loop()
     delay(1500);
 
     // Use WiFiClient class to create TCP connections
+    WiFiClient client;
 
     Serial.println();
     Serial.println("closing connection");
-
-    if (!client.connected())
-    {
-        reconnect();
-    }
-    client.loop();
-
-    long now = millis();
-    if (now - lastMsg > 2000)
-    {
-        lastMsg = now;
-        ++value;
-        snprintf(msg, 50, "hello world #%ld", value);
-        Serial.print("Publish message: ");
-        Serial.println(msg);
-        client.publish("outTopic", msg);
-    }
 }
